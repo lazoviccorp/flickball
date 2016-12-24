fd096bb0e5c4ce39d2078099a88306bc99e49497
Filip Lazovic <lazoviccorp@gmail.com>
2016-12-24 18:24:03 +0100

    updating socket

--- public/sketch.js
+++ public/sketch.js
@@ -31,7 +31,7 @@ function setup() {
   var c = createCanvas(windowWidth, windowHeight);
   c.position(0, 0);
 
-  socket = io.connect("https://138.68.164.5");
+  socket = io.connect("https://138.68.168.248");
 
   drawBackground();
   createDialog();
